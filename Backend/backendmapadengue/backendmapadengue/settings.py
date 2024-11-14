4e99f1926d40dc9e9c2db260a288a5480027ea15
Breno Henrique <brenohenriqueblizzard@gmail.com>
2024-11-14 19:16:37 -0300

    remoção de key

--- Backend/backendmapadengue/backendmapadengue/settings.py
+++ Backend/backendmapadengue/backendmapadengue/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4^2153p8%-bask9b)t44xg@*(ob^s5-@@1gfzi(q(gv&%9h=f^'
+SECRET_KEY =''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
